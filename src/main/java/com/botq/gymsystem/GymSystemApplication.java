864143cf94aa46e8d2c52a646228e67eaeff4691
Patryk Szwed <patryk.szwed@outlook.com>
2019-01-13 17:55:54 +0100

    initial commit

---
+++ src/main/java/com/botq/gymsystem/GymSystemApplication.java
@@ -0,0 +1,14 @@
+package com.botq.gymsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymSystemApplication.class, args);
+    }
+
+}
+
