a0d09de0b98d22a7517b886e373bff8f3e9a70e0
patryk-szwed <patryk.szwed@outlook.com>
2019-01-14 21:06:10 +0100

    Removed useless files for that moment

--- src/main/java/com/botq/gymsystem/controllers/IndexController.java
+++
@@ -1,9 +0,0 @@
-package com.botq.gymsystem.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(value = {"","/","/index","/home"})
-public class IndexController {
-}
