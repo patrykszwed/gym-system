5cb4884a7950b581bcfdbc318bee91a1b7839f96
patryk-szwed <patryk.szwed@outlook.com>
2019-01-15 23:18:43 +0100

    Lombok removed

--- src/main/java/com/botq/gymsystem/domain/Exercise.java
+++ src/main/java/com/botq/gymsystem/domain/Exercise.java
@@ -1,18 +1,11 @@
 package com.botq.gymsystem.domain;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
-@NoArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Exercise {
 
     @Id
@@ -31,6 +24,50 @@ public class Exercise {
             joinColumns = @JoinColumn(name = "exercise_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> userList = new ArrayList<>();
+
+    public Exercise(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getExerciseId() {
+        return exerciseId;
+    }
+
+    public void setExerciseId(String exerciseId) {
+        this.exerciseId = exerciseId;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
 }
 
 //todo Is something like Backlog needed? For example for tracking series and repetitions each day.
--- src/main/java/com/botq/gymsystem/domain/User.java
+++ src/main/java/com/botq/gymsystem/domain/User.java
@@ -1,9 +1,6 @@
 package com.botq.gymsystem.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -13,9 +10,6 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Data
-@NoArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class User {
 
     @Id
@@ -30,12 +24,64 @@ public class User {
     private String firstName;
     private String lastName;
 
+    //todo change Date to LocalDate in each model?
     @JsonFormat(pattern = "yyyy-mm-dd")
     @Column(updatable = false)
     private Date registrationDate;
 
     @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "userList")
     private List<Exercise> exerciseList = new ArrayList<>();
+
+    public User(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public List<Exercise> getExerciseList() {
+        return exerciseList;
+    }
+
+    public void setExerciseList(List<Exercise> exerciseList) {
+        this.exerciseList = exerciseList;
+    }
 
     //todo is date broken while adding new entry?
     @PrePersist
